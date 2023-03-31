87739f3e4707123fcf65e7546d4c5207387480df
AllenJi <rtbdgf@163.com>
2023-03-31 09:16:32 +0800

    clean code

--- CSharpTest/Form1.cs
+++ CSharpTest/Form1.cs
@@ -6,16 +6,5 @@ namespace CSharpTest
         {
             InitializeComponent();
         }
-
-        private void calculate_Click(object sender, EventArgs e)
-        {
-            int money = Convert.ToInt32(moneyText.Text);
-            int year = Convert.ToInt32(yearText.Text);
-            double rate = Convert.ToDouble(rateText.Text) / 100;
-            double interest = money * rate * year;
-            interestText.Text = interest.ToString();
-            double total = money + interest;
-            totalText.Text = total.ToString();
-        }
     }
 }
