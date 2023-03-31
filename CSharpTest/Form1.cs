b40db61379fe317dd529965550f1e4e563afc7c0
AllenJi <rtbdgf@163.com>
2023-03-31 10:50:50 +0800

    update

--- CSharpTest/Form1.cs
+++ CSharpTest/Form1.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace CSharpTest
 {
     public partial class Form1 : Form
@@ -5,6 +7,27 @@ namespace CSharpTest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void findButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 2; i < 1000; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j <= i / 2; j++)
+                    if (i % j == 0)
+                        sum += j;
+                if (sum == i)
+                {
+                    sb.Append(i + " 是一个完数：" + i + "=1");
+                    for (int j = 2; j <= i / 2; j++)
+                        if (i % j == 0)
+                            sb.Append("+" + j);
+                    sb.Append('\n');
+                }
+            }
+            outputLabel.Text = sb.ToString();
         }
     }
 }
