ecfd3c3f9ada02bf6d84b8e73b385b259f29416f
AllenJi <rtbdgf@163.com>
2023-03-31 10:52:47 +0800

    update

--- CSharpTest/Form1.Designer.cs
+++ CSharpTest/Form1.Designer.cs
@@ -28,57 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            findButton = new Button();
-            outputLabel = new Label();
             SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(44, 87);
-            label1.Name = "label1";
-            label1.Size = new Size(149, 24);
-            label1.TabIndex = 0;
-            label1.Text = "1000 以内的完数";
-            // 
-            // findButton
-            // 
-            findButton.Location = new Point(277, 82);
-            findButton.Name = "findButton";
-            findButton.Size = new Size(112, 34);
-            findButton.TabIndex = 1;
-            findButton.Text = "查找";
-            findButton.UseVisualStyleBackColor = true;
-            findButton.Click += findButton_Click;
-            // 
-            // outputLabel
-            // 
-            outputLabel.AutoSize = true;
-            outputLabel.Location = new Point(44, 166);
-            outputLabel.Name = "outputLabel";
-            outputLabel.Size = new Size(73, 24);
-            outputLabel.TabIndex = 2;
-            outputLabel.Text = "Output";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(11F, 24F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(outputLabel);
-            Controls.Add(findButton);
-            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Label label1;
-        private Button findButton;
-        private Label outputLabel;
     }
 }
--- CSharpTest/Form1.cs
+++ CSharpTest/Form1.cs
@@ -8,26 +8,5 @@ namespace CSharpTest
         {
             InitializeComponent();
         }
-
-        private void findButton_Click(object sender, EventArgs e)
-        {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 2; i < 1000; i++)
-            {
-                int sum = 0;
-                for (int j = 1; j <= i / 2; j++)
-                    if (i % j == 0)
-                        sum += j;
-                if (sum == i)
-                {
-                    sb.Append(i + " 是一个完数：" + i + "=1");
-                    for (int j = 2; j <= i / 2; j++)
-                        if (i % j == 0)
-                            sb.Append("+" + j);
-                    sb.Append('\n');
-                }
-            }
-            outputLabel.Text = sb.ToString();
-        }
     }
 }
